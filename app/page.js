68fb1f3ff0a18ef8f48c9af5be9d7be7a75abfc2
Daniele-Biscari <biscari.daniele@gmail.com>
2024-01-02 17:32:53 +0100

    design: fine landing page

--- app/page.js
+++ app/page.js
@@ -1,6 +1,16 @@
-import Image from 'next/image'
+"use client"
+
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+  const router = useRouter()
+  
+  const goToLanding = true // questa variabile verrà inizializzata da un database in true in caso sia stato scelto "dove vorrei investire in /landing"
+  if (goToLanding){
+    router.push("/landing")
+  }
+  
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p>this is the homepage</p>
--- app/ui/fonts.js
+++ app/ui/fonts.js
@@ -1,7 +1,13 @@
-import { Inter, Lusitana } from 'next/font/google';
+import { Inter, IBM_Plex_Sans, Roboto } from 'next/font/google';
  
-export const inter = Inter({ subsets: ['latin'] });
-export const lusitana = Lusitana({ 
-    subsets: ['latin'], 
-    weight: ['400', '700']
-});
+export const ibmPlexSans = IBM_Plex_Sans({ 
+    subsets: ['latin'],
+    weight: ["100", "200", "300", "400", "500", "600", "700"]
+})
+
+export const inter = Inter({subsets: ['latin']})
+
+export const roboto = Roboto({
+    subsets: ['latin'],
+    weight: ["100", "300", "400", "500", "700", "900"]
+})
