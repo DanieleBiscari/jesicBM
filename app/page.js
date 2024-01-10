198253cb3a705868504f90b8cbac1c9b27faef65
Daniele-Biscari <biscari.daniele@gmail.com>
2024-01-10 19:33:02 +0100

    fix: fixing landing page warning with useEffect

--- app/page.js
+++ app/page.js
@@ -1,19 +1,22 @@
 "use client"
-
 import { useRouter } from 'next/navigation'
+import { useEffect } from 'react';
 
 export default function Home() {
-  const router = useRouter()
-  
+  const router = useRouter();
   const goToLanding = true // questa variabile verrà inizializzata da un database in true in caso sia stato scelto "dove vorrei investire in /landing"
-  if (goToLanding){
-    router.push("/landing")
-  }
-  
+   
+  useEffect(() => {
+    goToLanding ? router.push("/landing") : pass;
+  },[])
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>this is the homepage</p>
-    </main>
-  )
+  if (goToLanding){
+    return(<div>loading...</div>)
+  }else {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <p>this is the homepage</p>
+      </main>
+    )
+  }
 }
